059917616406293c287b79481ca225cff0f7ba65
JarvisJiang <jiang115jie@gmail.com>
2015-07-27 22:53:28 +0800

    修改首页样式

--- web/dist/js/index.js
+++ web/dist/js/index.js
@@ -5,7 +5,7 @@ var $ = window.$ || window.Zepto;
 
 require('./mylib/tabs');
 
-$('.page > .main').height($(document.body).height() - $('.page > header').height() - $('.page > footer').height());
+$('.page > .main').height($(document.body).height() - $('.page > header').height() - $('.page > footer').height() - $('.page > .tabs').height());
 
 $('[data-cid]').on('tap', function() {
     var cid = this.dataset.cid;
--- web/src/browserify/index.js
+++ web/src/browserify/index.js
@@ -4,7 +4,7 @@ var $ = window.$ || window.Zepto;
 
 require('./mylib/tabs');
 
-$('.page > .main').height($(document.body).height() - $('.page > header').height() - $('.page > footer').height());
+$('.page > .main').height($(document.body).height() - $('.page > header').height() - $('.page > footer').height() - $('.page > .tabs').height());
 
 $('[data-cid]').on('tap', function() {
     var cid = this.dataset.cid;
